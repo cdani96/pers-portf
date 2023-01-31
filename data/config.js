e7f30a8efb7a3be6841840ad0bb906bdf54f7f82
Dani <120257212+cdani96@users.noreply.github.com>
2023-01-31 00:56:54 -0500

    Create config.js

---
+++ data/config.js
@@ -0,0 +1,105 @@
+export const meta = {
+
+  title: 'César Daniel - Web Developer and Designer',
+
+  description: 'Hey, I\'m Daniel. I\'m a web developer and designer building things people love.'
+
+}
+
+export const hero = {
+
+  title: "Hey there! I'm César Daniel.",
+
+  desc: "I'm a web developer and designer building things people love."
+
+};
+
+export const projects = {
+
+  title: 'My Projects',
+
+  desc: "Here are some of my favorite projects that I've worked on.",
+
+  projects: [
+
+    {
+
+      title: 'First Personal Page',
+
+      description: 'My first personal website that make me so happy',
+
+      link: 'https://cdani.github.io/my-project',
+
+      github: ''
+
+    },
+
+    {
+
+      title: 'My Actual Company Website',
+
+      description: 'Impoved the design of my company website!',
+
+      link: 'https://mathisa.co.cu',
+
+      github: ''
+
+    },
+
+    {
+
+      title: 'Another of my first websites',
+
+      description: 'A creepy website that made with love',
+
+      link: 'https://cphessp.cu',
+
+      github: ''
+
+    }
+
+  ]
+
+};
+
+export const stack = {
+
+  title: 'My Stack',
+
+  stack: [
+
+    'HTML',
+
+    'CSS',
+
+    'JavaScript',
+
+    'Typescript',
+
+    'React',
+
+    'NextJS',
+
+    'TailwindCSS',
+
+    'NodeJs',
+
+    'Git'
+
+  ]
+
+};
+
+export const contact = {
+
+  title: 'Contact Me',
+
+  email: 'cesardaniel.9611@gmail.com',
+
+  github: 'cdani96',
+
+  dribbble: 'cdaniel96',
+
+  linkedin: 'cesardaniel96'
+
+};
